79c05fcc82175eedc235cc66a165d365406410f8
Maira Iantas <mairacristinaiantas@gmail.com>
2017-11-12 23:15:29 +0000

    part 3

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Customer do
-  permit_params :full_name, :phone, :image, :email, :notes
+  permit_params :full_name, :phone, :image, :email, :notes, :province_id
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,5 @@
 class Customer < ApplicationRecord
   validate :full_name, :phone, :email, :notes
+
+  belongs_to :province
 end
